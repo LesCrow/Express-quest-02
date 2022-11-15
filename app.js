ecfc89ffa6de88a0e1a8a1168e73924e618014b4
LesCrow <john.baruteau@gmail.com>
2022-11-15 09:50:15 +0100

    Methode POST done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -19,6 +22,9 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
